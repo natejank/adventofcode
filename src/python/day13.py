b708fb92f0d7f5a5e38f62c68e9f3d5495f2062a
natejank <njj3397@rit.edu>
2022-12-13 20:29:43 -0500

    finished day 13 by using python

---
+++ src/python/day13.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+import sys
+
+
+def main(input_file: str):
+    packets = []
+    with open(input_file) as handle:
+        for line in handle:
+            if line == '\n':
+                pass
+            else:
+                packets.append(eval(line))
+
+    print(part1(packets))
+    print(part2(packets))
+
+
+def part1(packets):
+    output = 0
+    group = 0
+    for i in range(0, len(packets), 2):
+        group += 1
+        left = packets[i]
+        right = packets[i + 1]
+        result = compare(left, right)
+        if result > 0:
+            output += group
+    return output
+
+
+def part2(packets):
+    sorted_packets = []
+    divider_positions = []
+
+    for packet in packets:
+        for i in range(len(sorted_packets)):
+            if compare(packet, sorted_packets[i]) > 0:
+                sorted_packets.insert(i, packet)
+                break
+        else:
+            sorted_packets.append(packet)
+    for divider in [[[2]], [[6]]]:
+        for i in range(len(sorted_packets)):
+            if compare(divider, sorted_packets[i]) > 0:
+                sorted_packets.insert(i, divider)
+                divider_positions.append(i + 1)
+                break
+                
+    return divider_positions[0] * divider_positions[1]
+
+
+def compare(left, right):
+    max_len = min(len(left), len(right))
+    for i in range(max_len):
+        left_val = left[i]
+        right_val = right[i]
+        if type(left_val) == int and type(right_val) == int:
+            result = right_val - left_val
+        elif type(left_val) == list and type(right_val) == list:
+            result = compare(left_val, right_val)
+        elif type(left_val) == list:
+            result = compare(left_val, [right_val])
+        else:
+            result = compare([left_val], right_val)
+
+        if result != 0:
+            return result
+    return len(right) - len(left)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
