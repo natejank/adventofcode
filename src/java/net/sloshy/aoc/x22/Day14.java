c08a876d4553521ed3049e54b43f2f7864d843cc
natejank <njj3397@rit.edu>
2022-12-14 16:55:45 -0500

    part 1

---
+++ src/java/net/sloshy/aoc/x22/Day14.java
@@ -0,0 +1,167 @@
+package net.sloshy.aoc.x22;
+
+import java.util.*;
+
+import net.sloshy.aoc.common.Coordinate;
+import net.sloshy.aoc.common.Utilities;
+
+public class Day14 {
+    public static void main(String[] args) {
+        var day14 = new Day14(Utilities.getContent(args[0], (String s) -> {
+            List<Coordinate> vertex = new ArrayList<>();
+            var coords = s.split(" -> ");
+            for (var coord : coords) {
+                var coordinate = coord.split(",");
+                vertex.add(new Coordinate(Integer.parseInt(coordinate[0]), Integer.parseInt(coordinate[1])));
+            }
+            return vertex;
+        }));
+        Utilities.printResult(day14.part1(), 0);
+    }
+
+    private List<List<Coordinate>> input;
+
+    public Day14(List<List<Coordinate>> input) {
+        this.input = input;
+    }
+    public int part1() {
+        Cave cave = new Cave(input);
+        int sandCount = 0;
+        while (placeSand(cave, new Coordinate(500, 0))) {
+            sandCount++;
+        }
+        return sandCount;
+    }
+
+    public boolean placeSand(Cave cave, Coordinate sand) {
+        Coordinate down = new Coordinate(sand.x(), sand.y() + 1);
+        Coordinate left = new Coordinate(sand.x() - 1, sand.y() + 1);
+        Coordinate right = new Coordinate(sand.x() + 1, sand.y() + 1);
+
+        if (!cave.withinBounds(sand)) {
+            return false;
+        } else if (!cave.occupied(down)) {
+            return placeSand(cave, down);
+        } else if (cave.occupied(down)) {
+            if (!cave.occupied(left)) {
+                return placeSand(cave, left);
+            } else if (!cave.occupied(right)) {
+                return placeSand(cave, right);
+            } else {
+                cave.putSand(sand);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public class Cave {
+        private Set<Coordinate> rocks;
+        private Set<Coordinate> sand;
+        private Coordinate leftmost;
+        private Coordinate rightmost;
+        private Coordinate lowest;
+
+        public Cave(List<List<Coordinate>> rockVertices) {
+            rocks = new HashSet<>();
+            sand = new HashSet<>();
+            for (var vertex : rockVertices) {
+                for (int i = 0; i < vertex.size() - 1; i++) {
+                    if (leftmost == null)
+                        leftmost = vertex.get(i);
+                    if (rightmost == null)
+                        rightmost = vertex.get(i);
+                    if (lowest == null)
+                        lowest = vertex.get(i);
+
+                    Coordinate start = vertex.get(i);
+                    Coordinate end = vertex.get(i + 1);
+
+                    // ----- BEGIN SPAGHETTI -----
+                    if (start.x() < leftmost.x())
+                        leftmost = start;
+                    if (end.x() > rightmost.x())
+                        rightmost = end;
+                    if (start.x() > rightmost.x())
+                        rightmost = start;
+                    if (end.x() < leftmost.x())
+                        leftmost = end;
+                    if (start.y() > lowest.y())
+                        lowest = start;
+                    if (end.y() > lowest.y())
+                        lowest = end;
+                    // ----- END SPAGHETTI -----
+
+                    if (start.y() - end.y() == 0) {
+                        // horizontal
+                        boolean increasing = end.x() - start.x() > 0;
+                        for (int x = start.x(); increasing ? x <= end.x() : x >= end.x();) {
+                            rocks.add(new Coordinate(x, start.y()));
+
+                            if (increasing){
+                                x++;
+                            } else {
+                                x--;
+                            }
+                        }
+                    } else {
+                        // vertical
+                        boolean increasing = end.y() - start.y() > 0;
+                        for (int y = start.y(); increasing ? y <= end.y() : y >= end.y();) {
+                            rocks.add(new Coordinate(start.x(), y));
+                            if (increasing) {
+                                y++;
+                            } else {
+                                y--;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        /**
+         * FILL YOUR PC WITH SAND
+         *
+         * @param position position to fill with sand
+         */
+        public void putSand(Coordinate position) {
+            sand.add(position);
+        }
+
+        public boolean rockAt(Coordinate position) {
+            return rocks.contains(position);
+        }
+
+        public boolean sandAt(Coordinate position) {
+            return sand.contains(position);
+        }
+
+        public boolean withinBounds(Coordinate position) {
+            if (position.x() < leftmost.x() || position.x() > rightmost.x())
+                return false;
+            if (position.y() > lowest.y())
+                return false;
+            return true;
+        }
+
+        public boolean occupied(Coordinate position) {
+            return rockAt(position) || sandAt(position);
+        }
+
+        public void printMap() {
+            for (int row = 0; row <= 9; row++) {
+                for (int column = 494; column <= 503; column++) {
+                    Coordinate c = new Coordinate(column, row);
+                    if (sandAt(c))
+                        System.out.print('o');
+                    else if (rockAt(c))
+                        System.out.print('#');
+                    else
+                        System.out.print('.');
+                }
+                System.out.println();
+            }
+        }
+    }
+}
